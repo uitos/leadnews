7624b433a9eca4b621e92d5dbc63aec54fd96db8
orgin <484204799@qq.com>
2024-10-18 18:04:20 +0800

    项目初始化首页显示

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ApArticleController.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ApArticleController.java
@@ -1,10 +1,16 @@
 package com.heima.article.controller.v1;
 
 import com.heima.article.service.ApArticleService;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.common.dtos.ResponseResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author ghy
@@ -19,4 +25,21 @@ public class ApArticleController {
     @Autowired
     private ApArticleService apArticleService;
 
+    @PostMapping("load")
+    public ResponseResult load(ArticleHomeDto dto) {
+
+        return apArticleService.getByArticleHomeDto(dto, null);
+    }
+
+    @PostMapping("loadnew")
+    public ResponseResult loadnew(ArticleHomeDto dto) {
+
+        return apArticleService.getByArticleHomeDto(dto, (short) 1);
+    }
+
+    @PostMapping("loadmore")
+    public ResponseResult loadmore(ArticleHomeDto dto) {
+
+        return apArticleService.getByArticleHomeDto(dto, (short) 2);
+    }
 }
--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
@@ -1,11 +1,22 @@
 package com.heima.article.service.impl;
 
+import com.alibaba.cloud.commons.lang.StringUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.heima.article.mapper.ApArticleMapper;
 import com.heima.article.service.ApArticleService;
+import com.heima.common.constants.ArticleConstants;
+import com.heima.common.exception.CustomException;
+import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
+import org.apache.commons.net.nntp.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -21,4 +32,30 @@ public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper, ApArticle
     @Autowired
     private ApArticleMapper apArticleMapper;
 
+    @Override
+    public ResponseResult getByArticleHomeDto(ArticleHomeDto dto, Short type) {
+        if (Objects.isNull(dto)) {
+            throw new CustomException(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        Integer size = dto.getSize();
+        if (Objects.isNull(size) || size <= 0 || size > 30) {
+            dto.setSize(10);
+        }
+        String tag = dto.getTag();
+        if (StringUtils.isBlank(tag)) {
+            dto.setTag(ArticleConstants.DEFAULT_TAG);
+        }
+        Date maxBehotTime = dto.getMaxBehotTime();
+        if (Objects.isNull(maxBehotTime)) {
+            dto.setMaxBehotTime(new Date());
+        }
+        Date minBehotTime = dto.getMinBehotTime();
+        if (Objects.isNull(minBehotTime)) {
+            dto.setMinBehotTime(new Date());
+        }
+        List<ApArticle> apArticles = apArticleMapper.selectList(dto, null);
+
+        return ResponseResult.okResult(apArticles);
+    }
 }
+
