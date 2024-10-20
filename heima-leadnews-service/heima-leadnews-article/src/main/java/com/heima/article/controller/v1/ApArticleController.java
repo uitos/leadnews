c4e7531186b961a5756609b4f60ca2b74e97c749
18836009384 <3406883097@qq.com>
2024-10-20 08:18:10 +0800

    修改请求方法

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ApArticleController.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ApArticleController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,21 +28,21 @@ public class ApArticleController {
     private ApArticleService apArticleService;
 
     @ApiOperation("加载首页")
-    @RequestMapping("/load")
+    @PostMapping("/load")
     public ResponseResult load(@RequestBody ArticleHomeDto dto) {
         log.warn("加载首页:{}", dto);
         return apArticleService.load(dto,ArticleConstants.LOADTYPE_LOAD_NEW);
     }
 
     @ApiOperation("加载最新")
-    @RequestMapping("/loadnew")
+    @PostMapping("/loadnew")
     public ResponseResult loadnew(@RequestBody ArticleHomeDto dto) {
         log.warn("加载最新:{}", dto);
         return apArticleService.load(dto, ArticleConstants.LOADTYPE_LOAD_NEW);
     }
 
     @ApiOperation("加载更多")
-    @RequestMapping("/loadmore")
+    @PostMapping("/loadmore")
     public ResponseResult loadmore(@RequestBody ArticleHomeDto dto) {
         log.warn("加载更多:{}", dto);
         return apArticleService.load(dto, ArticleConstants.LOADTYPE_LOAD_MORE);
