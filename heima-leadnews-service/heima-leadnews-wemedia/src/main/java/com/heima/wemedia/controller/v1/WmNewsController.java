bcdde449aa2ade17055dade9c4db024d56994bbd
gua <3481678691@qq.com>
2024-10-21 14:25:15 +0800

    文章发布实现

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
@@ -1,14 +1,12 @@
 package com.heima.wemedia.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsDto;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
 import com.heima.wemedia.service.WmNewsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/news")
@@ -20,9 +18,26 @@ public class WmNewsController {
     @Autowired
     private WmNewsService wmNewsService;
 
+    /**
+     *根据id查询
+     * @param id
+     * @return
+     */
+    @PostMapping("/submit")
+   public ResponseResult submit(Boolean draft ,@RequestBody WmNewsDto dto) {
+        log.warn("draft:{} , dto:{}", draft , dto);
+        return wmNewsService.submit(draft , dto);
+    }
+
+    @GetMapping("/one/{id}")
+    public ResponseResult one(@PathVariable("id") Integer id) {
+        log.warn("查询文章id:{}", id);
+        return ResponseResult.okResult(wmNewsService.getById(id));
+    }
+
     @PostMapping("/list")
-    public ResponseResult list(@RequestBody WmNewsPageReqDto dto){
-        log.warn("dto:{}" , dto);
+    public ResponseResult list(@RequestBody WmNewsPageReqDto dto) {
+        log.warn("dto:{}", dto);
         return wmNewsService.pageQuery(dto);
 
     }
--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
@@ -1,25 +1,51 @@
 package com.heima.wemedia.service.impl;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.nacos.client.naming.utils.CollectionUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.common.constants.WemediaConstants;
 import com.heima.common.exception.CustomException;
 import com.heima.common.utils.UserContext;
+import com.heima.model.article.dtos.ContentDto;
 import com.heima.model.common.dtos.PageResponseResult;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.wemedia.dtos.WmNewsDto;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
+import com.heima.model.wemedia.pojos.WmMaterial;
 import com.heima.model.wemedia.pojos.WmNews;
+import com.heima.model.wemedia.pojos.WmNewsMaterial;
+import com.heima.wemedia.WemediaApplication;
+import com.heima.wemedia.mapper.WmMaterialMapper;
 import com.heima.wemedia.mapper.WmNewsMapper;
+import com.heima.wemedia.mapper.WmNewsMaterialMapper;
 import com.heima.wemedia.service.WmNewsService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import springfox.documentation.spring.web.json.Json;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> implements WmNewsService {
+
+    @Autowired
+    private WmNewsMaterialMapper wmNewsMaterialMapper;
+    @Autowired
+    private WmMaterialMapper wmMaterialMapper;
 
     //分页条件查询
     @Override
@@ -45,5 +71,107 @@ public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> impleme
         PageResponseResult pageResponseResult = new PageResponseResult((int) page.getCurrent(), (int) page.getSize(), (int) page.getTotal());
         pageResponseResult.setData(page.getRecords());
         return pageResponseResult;
+    }
+
+    @Override
+    @Transactional
+    public ResponseResult submit(Boolean draft, WmNewsDto dto) {
+        Long wmUserId = UserContext.getId();
+        if (Objects.isNull(wmUserId)){
+            throw new CustomException(AppHttpCodeEnum.NEED_LOGIN);
+        }
+        if (!Objects.isNull(dto.getId())){
+            //更新删除关联关系
+            wmNewsMaterialMapper.delete(new LambdaQueryWrapper<WmNewsMaterial>()
+                    .eq(WmNewsMaterial::getNewsId, dto.getId()));
+        }
+        //保存或更新文章
+        WmNews wmNews = createWmNews(dto);
+        boolean flag = saveOrUpdate(wmNews);
+        if (!flag){
+            throw new CustomException(AppHttpCodeEnum.SERVER_ERROR);
+        }
+        if (!Objects.isNull(dto.getStatus())&&dto.getStatus()==0){
+            return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
+        }
+        //不是草稿，保存文章与素材的关系
+        //内容与素材的关系
+        List<String>  contentImageUrls= getImagesFromContent(wmNews.getContent());
+        saveNewsandMaterialRelations(wmNews.getId() , contentImageUrls , WemediaConstants.WM_CONTENT_REFERENCE);
+        //图片与素材的关系
+        List<String> coverImageUrls = dto.getImages();
+        saveNewsandMaterialRelations(wmNews.getId() , coverImageUrls , WemediaConstants.WM_COVER_REFERENCE);
+        return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
+    }
+    /**
+     * 构建自媒体文章实体
+     *
+     */
+    private WmNews createWmNews(WmNewsDto dto) {
+        WmNews wmNews = new WmNews();
+        BeanUtils.copyProperties(dto, wmNews);
+        wmNews.setUserId(UserContext.getId().intValue());
+        wmNews.setCreatedTime(new Date());
+        wmNews.setSubmitedTime(new Date());
+        List<String> imageUrls = dto.getImages();
+        if (WemediaConstants.WM_NEWS_TYPE_AUTO.equals(dto.getType())){
+            //封面自动选择
+            imageUrls = getImagesFromContent(dto.getContent());
+            if (CollectionUtils.isEmpty(imageUrls)){
+                wmNews.setType(WemediaConstants.WM_NEWS_NONE_IMAGE);
+            }else if(imageUrls.size() >=WemediaConstants.WM_NEWS_MANY_IMAGE) {
+                //三图
+                wmNews.setType(WemediaConstants.WM_NEWS_MANY_IMAGE);
+                imageUrls = imageUrls.stream().limit(WemediaConstants.WM_NEWS_MANY_IMAGE).collect(Collectors.toList());
+            } else {
+                //单图
+                wmNews.setType(WemediaConstants.WM_NEWS_SINGLE_IMAGE);
+                imageUrls = imageUrls.stream().limit(WemediaConstants.WM_NEWS_SINGLE_IMAGE).collect(Collectors.toList());
+            }
+            dto.setImages(imageUrls);
+        }
+        wmNews.setImages(StringUtils.join(imageUrls, ","));
+        wmNews.setEnable((short) 1);
+        return wmNews;
+    }
+
+    private List<String> getImagesFromContent(String content) {
+        if (StringUtils.isBlank(content)){
+            return new ArrayList<>();
+        }else {
+            List<ContentDto> list = JSON.parseArray(content, ContentDto.class);
+            return list.stream()
+                    //过滤 type = "image"的数据
+                    .filter(item -> WemediaConstants.WM_NEWS_TYPE_IMAGE.equals(item.getType()))
+                    //获取value的值
+                    .map(ContentDto::getValue)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private void saveNewsandMaterialRelations(Integer newsId, List<String> imageUrls, Short type) {
+        if (CollectionUtils.isEmpty(imageUrls)){
+            return;
+        }
+        //根据图片url查询素材列表
+        List<Integer> materialIds = wmMaterialMapper
+                .selectList(new LambdaQueryWrapper<WmMaterial>().in(WmMaterial::getUrl, imageUrls))
+                .stream()
+                .map(WmMaterial::getId)
+                .collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(imageUrls)){
+            return;
+        }
+
+        int row = 0;
+        try {
+            //保存关联关系
+            row = wmNewsMaterialMapper.saveNewsandMaterialRelations(newsId, materialIds, type);
+        } catch (Exception e) {
+            throw new CustomException(AppHttpCodeEnum.SERVER_ERROR);
+        }
+        if (row ==0){
+            throw new CustomException(AppHttpCodeEnum.SERVER_ERROR);
+        }
     }
 }
